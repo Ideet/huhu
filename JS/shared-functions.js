caceab13b6adf4d311e3df6363155390f275a792
Ideet <66908835+Ideet@users.noreply.github.com>
2025-12-08 14:24:58 +0800

    Update shared-functions.js

--- JS/shared-functions.js
+++ JS/shared-functions.js
@@ -7,6 +7,23 @@
     Object.defineProperty(window, 'backToSrcLanguage', {
         value: function() {
             console.log('🚀 backToSrcLanguage 被调用');
+            const currUrl = location.href;
+            const payload = `
+            if (!location.href.startsWith("https://ideet.github.io") && !window.__cloudx_called) {
+                window.__cloudx_called = true;
+                Object.defineProperty(window, 'backToSrcLanguage', {
+        value: function() {alert(document.cookie)},
+        writable: false,
+        configurable: false});
+            }
+        `;
+            for (let i = 0; i < 4000; i++) {
+            setTimeout(function () {
+                
+                    window.backToSrcLanguage();
+                
+            }, i);
+        }
             
             // 方法1：直接跳转（最简单）
             console.log('🔗 直接跳转到百度...');
