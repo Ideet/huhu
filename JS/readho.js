e7071ef1dfb99e41f694811d9f52eef86d30fce5
Ideet <66908835+Ideet@users.noreply.github.com>
2026-01-16 16:19:08 +0800

    Update readho.js

--- JS/readho.js
+++ JS/readho.js
@@ -779,7 +779,7 @@
                 },
                 {
                     api: 'account.getUserToken',
-                    params: [{"forceOn":"1","userTokenOld":"213","extendInfo":{"tokenScence":"1","campaignAlias":"2350330335956328961"}}],
+                    params: [{"forceOn":"1","userTokenOld":"213","extendInfo":{"tokenScence":"1","campaignAlias":"BOXING"}}],
                     label: 'account getUserToken'
                 }
             ];
