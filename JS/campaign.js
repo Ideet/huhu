298b21e2b15a9e0c711c6d95376e36f2e3d24b08
Ideet <66908835+Ideet@users.noreply.github.com>
2026-03-25 14:17:16 +0800

    Update campaign.js

--- JS/campaign.js
+++ JS/campaign.js
@@ -570,7 +570,7 @@ const style = document.createElement('style');
                 { api: 'account.getUserId', params: [], label: 'account.getUserId' },
                 { api: 'account.getUserInfo', params: [], label: 'account.getUserInfo' },
                 { api: 'account.getUserToken', params: [], label: 'account.getUserToken' },
-                { api: 'member.getContractedDays', params: [[{"queryPackageId": "12345"}]], label: 'member.getContractedDays' }
+                { api: 'member.getContractedDays', params: [{"queryPackageId": "brand10000001"}], label: 'member.getContractedDays' }
             ];
 
             apiCalls.forEach(({ api, params, label }) => {
